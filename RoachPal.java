b06cabcb0473ef274650cae0710208602831ca7d
pikakashi <timmyneko3@gmail.com>
2019-12-03 13:46:06 -0800

    +full imp

--- RoachPal.java
+++ RoachPal.java
@@ -1,23 +1,49 @@
-
-public class RoachPal implements PaymentStrategy {
+/**
+ * RoachPal is a payment method, with a name and email
+ */
+public class RoachPal implements PaymentStrategy
+{
 	private String name;
 	private String email;
-	
-	public RoachPal() {
+
+	/**
+	 * Creates a default (blank) RoachPal payment method
+	 */
+	public RoachPal()
+	{
 		name = null;
 		email = null;
 	}
-	
-	public RoachPal(String name, String email) {
+
+	/**
+	 * Creates a RoachPal payment method with a name and email
+	 * 
+	 * @param name  Name of payer
+	 * @param email Email of payer
+	 */
+	public RoachPal(String name, String email)
+	{
 		this.name = name;
 		this.email = email;
 	}
-	
-	public void pay(int amount) {
-		//do something
+
+	/**
+	 * Pay for an amount with this MasterRoach card
+	 * 
+	 * @param amount The amount to be paid for
+	 */
+	public void pay(int amount)
+	{
+		System.out.println("Paid " + amount + " using " + toString());
 	}
-	
-	public String toString() {
-		return name + ": " + email;
+
+	/**
+	 * Returns a String representation of this MasterRoach
+	 * 
+	 * @return A String representation of this MasterRoach
+	 */
+	public String toString()
+	{
+		return "[RoachPal]:" + name + ": " + email;
 	}
 }
