e583cf18a4619e477d176d1afb8232a90493bb0d
pikakashi <timmyneko3@gmail.com>
2019-12-03 12:26:53 -0800

    +vacancy improvements, +ability to add rooms

--- RoachMotel.java
+++ RoachMotel.java
@@ -5,7 +5,6 @@ public class RoachMotel
 	private static RoachMotel instance = null;
 	private Room[] rooms;
 	private static final int NUMBER_OF_ROOMS = 5;
-	private boolean vacancy;
 
 	/**
 	 * Creates a RoachMotel.
@@ -13,7 +12,6 @@ public class RoachMotel
 	private RoachMotel()
 	{
 		rooms = new Room[NUMBER_OF_ROOMS];
-		vacancy = true;
 	}
 
 	/**
@@ -37,7 +35,27 @@ public class RoachMotel
 	 */
 	public boolean hasVacancy()
 	{
-		return vacancy;
+		for (int i = 0; i < NUMBER_OF_ROOMS ; i++)
+		{
+			if ( rooms[i] == null )
+			{
+				return true
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Set a room in the RoachMotel.
+	 * 
+	 * @param i  Room number [0-limit)
+	 * @param rm The room to set
+	 */
+	public void setRoom(int i, Room rm)
+	{
+		if (i >= NUMBER_OF_ROOMS)
+			return;
+		rooms[i] = rm;
 	}
 
 	/**
@@ -47,7 +65,7 @@ public class RoachMotel
 	 */
 	public String toString()
 	{
-		if (vacancy)
+		if (hasVacancy())
 			return "RoachMotel: Open Rooms";
 		else
 			return "RoachMotel: No Vacancy";
